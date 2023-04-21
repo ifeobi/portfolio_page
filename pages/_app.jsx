dfd02ae3b479cb4ec5875904dc7a20d974b369c8
abc <abc@example.com>
2023-04-21 23:21:43 +0100

    vercel analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,13 +1,13 @@
 import "../styles/global.css";
 import { useRouter } from "next/router";
-// import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
   return (
     <>
       <Component {...pageProps} />
-      {/* <Analytics /> */}
+      <Analytics />
     </>
   );
 }
